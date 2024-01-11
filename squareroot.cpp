9d84339efccb549b86921ddda02d64c73f2cc21a
Calvin-Sebu <155639713+Calvin-Sebu@users.noreply.github.com>
2024-01-11 20:18:00 +0530

    Add files via upload

---
+++ squareroot.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+  int a;
+  cout<<"enter number to see root :"<<'\t';
+  cin>>a;
+  double sqrt(double a);
+  cout<<"root of"<<'\t'<<a<<'\t'<<"is"<<'\t'<<sqrt(a);
+  cout<<'\n'<<"PROGRAMED BY CALVIN";
+  return 0;
+}
+//This program is written by Calvin Sebu
+
+// You can edit or distribute 
+/*this is open source code*/
